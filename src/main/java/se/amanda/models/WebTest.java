c8df50978f2e1c2c48c8ae3666194642df78a3eb
knyttali <amanda_andrae@hotmail.com>
2022-03-31 18:11:14 +0200

    webbtest

---
+++ src/main/java/se/amanda/models/WebTest.java
@@ -0,0 +1,31 @@
+package se.amanda.models;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebTest {
+    public static void main(String[] args)throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver  = new ChromeDriver();
+        driver.get("https://biluppgifter.se/"); //navigering till adress
+
+        Thread.sleep(3 * 1000);
+
+        WebElement searchBox = driver.findElement(By.name("r"));
+
+        searchBox.sendKeys("abc230"); //skriver in i search
+        searchBox.submit();
+
+        Thread.sleep(3 * 1000);
+
+        WebElement carName = driver.findElement(By.className("card-title"));
+        System.out.println(carName.getText());
+        WebElement searchBtn = driver.findElement(By.id("modelregister-button"));
+        searchBtn.click();
+
+    }
+}
